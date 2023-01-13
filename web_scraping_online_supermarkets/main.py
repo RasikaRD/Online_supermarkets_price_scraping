3fc0c10c51ae617a4293f36346cf30e3e26d3954
RasikaRD <rasikarajith94@gmail.com>
2023-01-13 09:41:31 +0530

    price scarping updated

---
+++ web_scraping_online_supermarkets/main.py
@@ -0,0 +1,5 @@
+from price_compare import compare_prices
+
+laughs_coconut = 'https://scrape-sm1.github.io/site1/COCONUT%20market1super.html'
+glomark_coconut = 'https://glomark.lk/coconut/p/11624'
+compare_prices(laughs_coconut, glomark_coconut)
